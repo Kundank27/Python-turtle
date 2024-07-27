492aff11a0e063ec1b4366b38b4bd8fefba3a53d
Kundank27 <162008927+Kundank27@users.noreply.github.com>
2024-07-27 16:30:23 +0530

    Add files via upload

---
+++ Hanuman_ji.py
@@ -0,0 +1,404 @@
+import turtle as h
+h.hideturtle()
+h.bgcolor('black')
+h.speed(100)
+h.width(3)
+h.penup()
+h.goto(0,-300)
+h.pendown()
+h.fillcolor('orange')
+h.begin_fill()
+h.circle(400)
+h.end_fill()
+h.penup()
+h.goto(-175,250)
+h.pendown()
+h.right(110)
+h.forward(20)
+for i in range(25):
+    h.right(0.1)
+    h.forward(2)
+
+h.fillcolor("black")
+h.begin_fill()
+# h.width(1)
+h.left(75)
+h.forward(15)
+h.left(90)
+for i in range(15):
+    h.right(1)
+    h.forward(2)
+h.left(50)
+for i in range(20):
+    h.right(2)
+    h.forward(3)
+
+h.right(170)
+
+for i in range(20):
+    h.left(2)
+    h.forward(3)
+h.right(65)
+
+for i in range(23):
+    h.left(1)
+    h.forward(2)
+
+h.right(75)
+h.forward(10)
+h.right(90)
+
+
+for i in range (15):
+    h.left(4)
+    h.forward(1.5)
+
+h.end_fill()
+
+
+h.left(115)
+h.forward(5)
+for i in range(20):
+    h.left(4)
+    h.forward(1)
+h.forward(15)
+for i in range(21):
+    h.right(4)
+    h.forward(1)
+
+# h.forward(70)
+for i in range(35):
+    h.left(0.1)
+    h.forward(2)
+
+for i in range(52):
+    h.left(4)
+    h .forward(1.25)
+
+h.left(141)
+# h.forward(40)
+for i in range(30):
+    h.left(0.9)
+    h.forward(2)
+
+for i in range(15):
+    h.left(4)
+    h.forward(1.5)
+for i in range(15):
+    h.right(4)
+    h.forward(1)
+h.width(4)
+for i in range(20):
+    h.left(4)
+    h.forward(1.5)
+h.width(3)
+for i in range(40):
+    h.left(0.9)
+    h.forward(2)
+h.right(180)
+for i in range(37):
+    h.right(0.9)
+    h.forward(2)
+
+h.left(30)
+for i in range(35):
+    h.left(4)
+    h.forward(1)
+h.forward(15)
+
+for i in range(12):
+    h.right(4)
+    h.forward(1)
+
+h.forward(35)
+for i in range(20):
+    h.left(4)
+    h.forward(2)
+h.width(4)
+h.forward(20)
+h.width(3)
+
+
+
+# eyebrow
+h.penup()
+h.goto(-150,160)
+h.pendown()
+h.fillcolor('black')
+h.begin_fill()
+h.right(15)
+for i in range(20):
+    h.left(2.5)
+    h.forward(1.5)
+h.forward(7)
+for i in range(27):
+    h.right(3)
+    h.forward(2)
+h.right(160)
+for i in range(25):
+    h.left(3)
+    h.forward(2)
+h.right(30)
+h.forward(5)
+for i in range(20):
+    h.right(3.5)
+    h.forward(2)
+h.end_fill()
+# /eyes
+h.penup()
+h.goto(-135,130)
+h.pendown()
+h.right(135)
+h.fillcolor('black')
+h.begin_fill()
+h.right(15)
+for i in range(20):
+    h.left(2.5)
+    h.forward(1.5)
+h.forward(7)
+for i in range(17):
+    h.right(3)
+    h.forward(1)
+h.right(150)
+for i in range(20):
+    h.left(2)
+    h.forward(1.25)
+h.right(30)
+h.forward(5)
+for i in range(17):
+    h.right(3.5)
+    h.forward(2)
+h.end_fill()
+
+h.penup()
+h.goto(0,110)
+h.pendown()
+h.fillcolor('black')
+h.begin_fill()
+h.left(80)
+for i in range(20):
+    h.left(2.5)
+    h.forward(1.5)
+h.forward(7)
+for i in range(30):
+    h.right(3.5)
+    h.forward(3.75)
+
+h.right(190)
+for i in range(90):
+    h.right(0.5)
+    h.forward(1)
+
+for i in range(40):
+    h.right(4)
+    h.forward(1)
+
+h.right(180)
+for i in range(25):
+    h.right(3.5)
+    h.forward(2.5)
+h.forward(7)
+for i in range(20):
+    h.left(2.5)
+    h.forward(3.5)
+h.right(190)
+# ******************
+for i in range(20):
+    h.right(2.5)
+    h.forward(3.5)
+
+h.forward(7)
+for i in range(23):
+    h.left(3.5)
+    h.forward(2.5)
+
+h.right(180)
+for i in range(40):
+    h.left(4)
+    h.forward(1)
+
+for i in range(90):
+    h.left(0.5)
+    h.forward(0.9)
+
+h.left(235)
+
+for i in range(27):
+    h.left(3.5)
+    h.forward(3.75)
+
+h.forward(3)
+
+for i in range(18):
+    h.right(2.5)
+    h.forward(1.5)
+
+h.end_fill()
+
+h.penup()
+h.goto(-165,260)
+h.pendown()
+
+h.right(50)
+
+for i in range(60):
+    h.right(1)
+    h.forward(2)
+
+for i in range(25):
+    h.left(1.5)
+    h.forward(1)
+
+for i in range(50):
+    h.right(1)
+    h.forward(2)
+
+h.penup()
+h.goto(-160,270)
+h.pendown()
+
+h.right(-73)
+
+for i in range(60):
+    h.right(1)
+    h.forward(2)
+
+for i in range(25):
+    h.left(1.5)
+    h.forward(1)
+
+for i in range(50):
+    h.right(1)
+    h.forward(2)
+# h.right(60)
+
+
+
+
+h.penup()
+h.goto(30,150)
+h.pendown()
+h.left(140)
+for i in range(21):
+    h.right(4)
+    h.forward(4)
+h.forward(6)
+for i in range(28):
+    h.right(4)
+    h.forward(4)
+h.left(30)
+for i in range(20):
+    h.right(2.5)
+    h.forward(2)
+h.left(10)
+for i in range(15):
+    h.left(2)
+    h.forward(1)
+
+for i in range(26):
+    h.right(0.5)
+    h.forward(1.5)
+for i in range(20):
+    h.right(3)
+    h.forward(1.5)
+h.right(10)
+h.forward(3)
+
+for i in range(20):
+    h.right(3)
+    h.forward(1.5)
+
+h.penup()
+h.goto(40,148)
+h.pendown()
+h.left(300)
+for i in range(17):
+    h.right(4)
+    h.forward(4)
+for i in range(20):
+    h.right(6)
+    h.forward(4)
+
+h.penup()
+h.goto(55,115)
+h.pendown()
+h.left(120)
+h.width(10)
+for i in range(20):
+    h.left(1)
+    h.forward(1)
+
+h.width(9)
+h.forward(2)
+h.width(8)
+h.forward(2)
+h.width(7)
+for i in range(40):
+    h.right(4)
+    h.forward(1)
+h.width(6)
+h. forward(10)
+h.width(7)
+for i in range(20):
+    h.left(2)
+    h.forward(1)
+
+h.width(8)
+
+for i in range(40):
+    h.right(4)
+    h.forward(0.5)
+
+h.penup()
+h.goto(50,105)
+h.pendown()
+h.left(220)
+h.width(5)
+for i in range(3):
+    h.left(4)
+    h.forward(1)
+h.width(6)
+for i in range(10):
+    h.right(6)
+    h.forward(1.5)
+h.width(5)
+for i in range(5):
+    h.left(3)
+    h.forward(1)
+
+
+h.width(3)
+h.penup()
+h.goto(85,10)
+h.pendown()
+h.fillcolor('black')
+h.begin_fill()
+h.right(65)
+for i in range(40):
+    h.left(2.5)
+    h.forward(4)
+
+for i in range(20):
+    h.left(4)
+    h.forward(1)
+for i in range(40):
+    h.left(2.5)
+    h.forward(4)
+
+h.left(90)
+h.forward(5)
+h.left(90)
+for i in range(30):
+    h.right(3)
+    h.forward(4)
+for i in range(18):
+    h.right(4)
+    h.forward(1)
+h.right(30)
+for i in range(30):
+    h.right(2.3)
+    h.forward(4)
+h.end_fill()
+
+
+h.done()
